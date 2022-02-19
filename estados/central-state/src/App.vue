e95bfafeb0dd8c671b17256f9c7f83026621c9a1
Andres <andresdreamerf@gmail.com>
2022-02-19 17:16:40 -0400

    estados: central-state

---
+++ estados/central-state/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+      <contador></contador>
+  </div>
+</template>
+
+<script>
+import contador from './components/Contador.vue'
+export default {
+  components: { contador },
+  name: 'app',
+  data () {
+    return {
+     
+    }
+  }
+}
+</script>
---
+++ estados/central-state/src/components/Contador.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container mt-5">
+        <div class="card" style="width: 18rem;">
+            <div class="card-header">
+                <h1>{{cantidad}}</h1>
+            </div>
+            <div class="card-body">
+                <a href="#" @click="disminuir" class="btn btn-warning">-</a>
+                <a href="#" @click="aumentar" class="btn btn-info">+</a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    methods:{
+        aumentar(){
+            ++this.$store.state.cantidad;
+        },
+        disminuir(){
+            --this.$store.state.cantidad;
+        }
+    },
+    data(){
+        return {
+            
+        }
+    },
+    computed:{
+        cantidad(){
+            return this.$store.state.cantidad;
+        }
+    }
+}
+</script>
