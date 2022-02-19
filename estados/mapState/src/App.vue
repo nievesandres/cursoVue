3e975228bf80ee046ae66e39e131e76489214d22
Andres <andresdreamerf@gmail.com>
2022-02-19 18:17:37 -0400

    mapState

---
+++ estados/mapState/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+      <h1>Hola mi nombre es  {{nombre}}</h1>
+  </div>
+</template>
+
+<script>
+import {mapState} from 'vuex'//importamos mapState para usar las propiedades del state de un manera mas directa
+export default {
+  name: 'app',
+  data () {
+    return {
+     
+    }
+  },
+  computed:mapState(['nombre'])
+}
+</script>
