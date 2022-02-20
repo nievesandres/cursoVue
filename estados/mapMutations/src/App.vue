d22830b3de3d8849de031db7632bb542c406b82a
Andres <andresdreamerf@gmail.com>
2022-02-19 22:16:23 -0400

    estados: mapMutations

---
+++ estados/mapMutations/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+      <contador></contador>
+  </div>
+</template>
+
+<script>
+import {mapState} from 'vuex'//importamos mapState para usar las propiedades del state de un manera mas directa
+import contador from './components/Contador.vue'
+export default {
+  name: 'app',
+  components:{contador},
+  data () {
+    return {
+     
+    }
+  },
+  computed:mapState(['nombre'])
+}
+</script>
---
+++ estados/mapMutations/src/components/Contador.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container mt-4">
+        <h1>{{cantidad}}</h1>
+        <hr>
+        <button class="btn btn-danger" @click="reducir">-</button>
+        <button class="btn btn-success" @click="aumentar">+</button>
+    </div>
+</template>
+
+<script>
+import {mapState, mapMutations} from 'vuex'
+export default {
+    computed: mapState(['cantidad']),
+    methods: mapMutations(['aumentar','reducir'])
+}
+</script>
